c3cf769d60f32cfcad4ff6952cd49d58ed332015
BertMortier <bertmortier@hotmail.com>
2014-11-12 17:56:36 +0100

    Create meta_generateInitialPop.m

---
+++ template/meta_generateInitialPop.m
@@ -0,0 +1,20 @@
+function pop = meta_generateInitialPop(N)
+global limits
+global nofInts
+pop={};
+for i=1:size(limits,2)
+    if(isa(limits{i},'numeric'))
+        if(i>nofInts)
+            pop=[pop num2cell(limits{i}*rand(N,1))]
+        else
+            pop=[pop num2cell(round(limits{i}*rand(N,1)))];
+        end
+    else
+        if(size(limits{i},2)>0)
+            pop=[pop {limits{i}{randi(size(limits{i}),N,1)}}']
+        else
+            pop=[pop num2cell(zeros(N,1))]
+        end
+    end
+end
+end
