5a2cf8918e18e8871a2a7dcddf7de2053e6f023b
BertMortier <bertmortier@hotmail.com>
2014-11-13 17:48:16 +0100

    Update meta_co_singlepoint_crossover.m
    bugje er uit gehaald (de child had een veld te veel)

--- template/crossover_operators/meta_co_singlepoint_crossover.m
+++ template/crossover_operators/meta_co_singlepoint_crossover.m
@@ -1,5 +1,4 @@
-function NewChrom = meta_co_singlepoint_crossover( parent1, parent2 )
+function child = meta_co_singlepoint_crossover( parent1, parent2 )
     co_point = randi(size(parent1, 2));
-    NewChrom = [parent1(1, 1:co_point) parent2(1, co_point:end)];
+    child = [parent1(1, 1:co_point) parent2(1, co_point+1:end)];
 end
-
