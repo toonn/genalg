334fe9c62af69b30ff156c1a844c0110f15a71c6
BertMortier <bertmortier@hotmail.com>
2014-11-18 18:31:13 +0100

    Ik heb die twee crossover operators aangepast dat ze nu twee kinderen teruggeven, ik weet wel nog niet of het wel het juiste formaat is (het is nu in 1 matrix onder elkaar, maar dat aanpassen lijkt mij triviaal, ik heb wel nog maar enkel sequential constructive crossover getest, maar die andere zal ook wel werken :p

--- template/crossover_operators/co_edge_recombination_operator.m
+++ template/crossover_operators/co_edge_recombination_operator.m
@@ -1,48 +1,53 @@
-function child = co_edge_recombination_operator(parent1, parent2, ~)
+function children = co_edge_recombination_operator(parent1, parent2, ~)
 
 N=size(parent1,2);
 
 child=zeros(1,N);
-list=1:N;
+children=zeros(2,N);
 
-connectedCities=zeros(N,4);
+connectedCities_backup=zeros(N,4);
 
 for i=1:N
-    connectedCities(parent1(i),1:2)=[parent1(mod(i,N)+1) parent1(mod(i-2,N)+1)];
-    connectedCities(parent2(i),3:4)=[parent2(mod(i,N)+1) parent2(mod(i-2,N)+1)];
+    connectedCities_backup(parent1(i),1:2)=[parent1(mod(i,N)+1) parent1(mod(i-2,N)+1)];
+    connectedCities_backup(parent2(i),3:4)=[parent2(mod(i,N)+1) parent2(mod(i-2,N)+1)];
 end
 
 for i=1:N
-    connectedCities(i,:)=[unique(connectedCities(i,:),'first') zeros(1,4-size(unique(connectedCities(i,:),'first'),2))];
+    connectedCities_backup(i,:)=[unique(connectedCities_backup(i,:),'first') zeros(1,4-size(unique(connectedCities_backup(i,:),'first'),2))];
 end
-connectedCities;
+connectedCities_backup;
 %je moet met een random positie beginnen aangezien het op de beginpositie
-%is dat een nieuwe edge zal worden geï¿½ntroduceerd normaal gezien
-p=randi(N);
-child(1)=p;
-list(find(list==p))=0;
-for i=2:N
-    connectedCities(find(connectedCities==p))=0;
-    possibleNewCities=connectedCities(p,find(connectedCities(p,:)));
-    if(isempty(possibleNewCities))
-        p=list(datasample(find(list),1));
-    else
-        p=possibleNewCities(1);
-    end
-    for j=2:size(possibleNewCities,2)
-        k=possibleNewCities(j);
-        if(size(find(connectedCities(k,:)),2)<=size(find(connectedCities(p,:)),2))
-            if(size(find(connectedCities(k,:),2),2)==size(find(connectedCities(p,:)),2))
-                if(rand>0.5)
+%is dat een nieuwe edge zal worden geïntroduceerd normaal gezien
+for j=1:2
+    connectedCities=connectedCities_backup;
+    list=1:N;
+    p=randi(N);
+    child(1)=p;
+    list(find(list==p))=0;
+    for i=2:N
+        connectedCities(find(connectedCities==p))=0;
+        possibleNewCities=connectedCities(p,find(connectedCities(p,:)));
+        if(isempty(possibleNewCities))
+            p=list(datasample(find(list),1));
+        else
+            p=possibleNewCities(1);
+        end
+        for j=2:size(possibleNewCities,2)
+            k=possibleNewCities(j);
+            if(size(find(connectedCities(k,:)),2)<=size(find(connectedCities(p,:)),2))
+                if(size(find(connectedCities(k,:),2),2)==size(find(connectedCities(p,:)),2))
+                    if(rand>0.5)
+                        p=k;
+                    end
+                else
                     p=k;
                 end
-            else
-                p=k;
             end
         end
+        child(i)=p;
+        list(find(list==p))=0;
     end
-    child(i)=p;
-    list(find(list==p))=0;
+    children(j,:)=child;
 end
 
 end
--- template/crossover_operators/co_sequential_constructive_crossover.m
+++ template/crossover_operators/co_sequential_constructive_crossover.m
@@ -6,49 +6,54 @@
 
 
 %Expected representation: path
-function child = co_sequential_constructive_crossover(parent1, parent2, costMatrix, ~)
-% costMatrix=[999 75  99  9   35  63  8 ;
-%             51  999 86  46  88  29  20;
-%             100 5   999 16  28  35  28;
-%             20  45  11  999 59  53  49;
-%             86  63  33  65  999 76  72;
-%             36  53  89  31  21  999 52;
-%             58  31  43  67  52  60  999];
+% function children = co_sequential_constructive_crossover(parent1, parent2, costMatrix, ~)
+costMatrix=[999 75  99  9   35  63  8 ;
+            51  999 86  46  88  29  20;
+            100 5   999 16  28  35  28;
+            20  45  11  999 59  53  49;
+            86  63  33  65  999 76  72;
+            36  53  89  31  21  999 52;
+            58  31  43  67  52  60  999];
 
-% parent1=[1 5 7 3 6 4 2];
-% parent2=[1 6 2 4 3 5 7];
+parent1=[1 5 7 3 6 4 2];
+parent2=[1 6 2 4 3 5 7];
 
 parents=[parent1 ; parent2]';
 N=size(parent1,2);
-list=[0 2:N];
-child=[1 zeros(1,N-1)];
-p=1;
+children=zeros(2,N);
 
-for i=2:N
-    subsequentNodes=parents(mod(find(parents==p),N)+[1 ; N+1])
-    
-    if(isempty(find(list==subsequentNodes(1))))
-        subsequentNodes(1)=find(list,1);
-    end
-    
-    if(isempty(find(list==subsequentNodes(2))))
-        subsequentNodes(2)=find(list,1);
-    end
-    
-    cp1=costMatrix(p,subsequentNodes(1));
-    cp2=costMatrix(p,subsequentNodes(2));
-    
-    if(subsequentNodes(1)==subsequentNodes(2) || cp1<cp2)
-        child(i)=subsequentNodes(1);
-    else
-        if(cp1>cp2)
-            child(i)=subsequentNodes(2);
-        else
-            nextNode=subsequentNodes(round(rand));
-            child(i)=nextNode;
+for j=1:2
+    list=[0 2:N];
+    p=j;
+    child=[1 zeros(1,N-1)];
+    for i=2:N
+        subsequentNodes=parents(mod(find(parents==p),N)+[1 ; N+1]);
+        
+        if(isempty(find(list==subsequentNodes(1))))
+            subsequentNodes(1)=find(list,1);
         end
-    end 
-    list(child(i))=0;
-    p=child(i); 
+
+        if(isempty(find(list==subsequentNodes(2))))
+            subsequentNodes(2)=find(list,1);
+        end
+
+        cp1=costMatrix(p,subsequentNodes(1));
+        cp2=costMatrix(p,subsequentNodes(2));
+
+        if(subsequentNodes(1)==subsequentNodes(2) || cp1<cp2)
+            child(i)=subsequentNodes(1);
+        else
+            if(cp1>cp2)
+                child(i)=subsequentNodes(2);
+            else
+                nextNode=subsequentNodes(round(rand));
+                child(i)=nextNode;
+            end
+        end 
+        list(child(i))=0;
+        p=child(i);
+    end
+    children(j,:)=child;
 end
-end
+    
+% end
